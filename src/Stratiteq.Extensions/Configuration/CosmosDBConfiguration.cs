478d6886eca1864d8a3f6ff3f58ae3084f077803
Peter Nylander <peter.nylander@stratiteq.com>
2020-04-06 11:05:42 +0200

    Fixed namespace issues (#3)
    Upped version to 1.0.2

--- src/Stratiteq.Extensions/Configuration/CosmosDBConfiguration.cs
+++ src/Stratiteq.Extensions/Configuration/CosmosDBConfiguration.cs
@@ -5,7 +5,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace Stratiteq.MicroserviceTemplate.Configurations
+namespace Stratiteq.Extensions.Configuration
 {
     public class CosmosDBConfiguration : IValidatableObject
     {
